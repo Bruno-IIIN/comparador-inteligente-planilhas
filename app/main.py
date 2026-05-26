a963f532e25c73e1d8dac907aedac85c03670f45
BRUNIIIN <brunofranciscoalvescpj@gmail.com>
2026-05-26 14:14:15 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
-from qt_material import apply_stylesheet
 
 from app.ui.main_window import MainWindow
 from app.ui.splash_screen import show_splash
 
+from qt_material import apply_stylesheet
+
 
 def main():
+
     app = QApplication(sys.argv)
 
-    apply_stylesheet(app, theme='dark_teal.xml')
+    apply_stylesheet(
+        app,
+        theme='dark_teal.xml'
+    )
 
     splash = show_splash(app)
 
     window = MainWindow()
+
     window.show()
 
     splash.finish(window)
